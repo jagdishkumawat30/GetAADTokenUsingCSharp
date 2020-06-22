6d4ddde44fe49a1f1330cbe47138279b3422109c
JD Bots <jdbots@dewirideTechnologies.onmicrosoft.com>
2020-06-22 10:44:02 +0530

    Add project files.

---
+++ AzureDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1
+{
+    public static class AzureDetails
+    {
+        public static string ClientID = "<Client ID or App ID>";
+        public static string ClientSecret = "<Client Secret or Password>";
+        public static string TenantID = "<Tenant ID>";
+        public static string AccessToken { get; internal set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetAuthorizationToken();
+            Console.WriteLine(AzureDetails.AccessToken);
+        }
+
+        public static void GetAuthorizationToken()
+        {
+            ClientCredential cc = new ClientCredential(AzureDetails.ClientID, AzureDetails.ClientSecret);
+            var context = new AuthenticationContext("https://login.microsoftonline.com/" + AzureDetails.TenantID);
+            var result = context.AcquireTokenAsync("https://management.azure.com/", cc);
+            if (result == null)
+            {
+                throw new InvalidOperationException("Failed to obtain the Access token");
+            }
+            AzureDetails.AccessToken = result.Result.AccessToken;
+        }
+    }
+}
